84ed9f8c66645aa589553ad0c39d9bb4fed037a0
Tarek-Kacem <79226060+Tarek-Kacem@users.noreply.github.com>
2021-03-11 17:49:06 +0100

    first commit

---
+++ src/componenets/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+            <h3>20 Rue Hessan Kacem Zaouiet Kontech</h3>
+        </div>
+    )
+}
+
+export default Address
